edb33cd0172beb0d0a547f7d144b182e252de281
edhowland <ed.howland@gmail.com>
2016-03-10 22:15:12 +0000

    \'added file/path, offenses to buffer in load_cop_json command\'

--- lib/tools/load_cop_json.rb
+++ lib/tools/load_cop_json.rb
@@ -2,5 +2,14 @@
 
 def load_cop_json buffer, path
   buffer.ins "Rubocop analyzer results\n"
+  contents = File.read(path)
+  loaded = JSON.load(contents)
+  loaded['files'].each do |file|
+    buffer.ins "#{file['path']}\n" 
+    file['offenses'].each do |offense|
+      buffer.ins offense.inspect
+      buffer.ins "\n"
+    end
+  end
 end
 
