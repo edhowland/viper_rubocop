a65d5b32fb2d6f5d09e6422fde60f10ba0202afc
edhowland <ed.howland@gmail.com>
2016-03-11 01:34:42 +0000

    \'Use F: for files and tab indentation for offenses for each file. Also reject files with empty offenses\'

--- lib/tools/load_cop_json.rb
+++ lib/tools/load_cop_json.rb
@@ -4,10 +4,10 @@ def load_cop_json buffer, path
   buffer.ins "Rubocop analyzer results\n"
   contents = File.read(path)
   loaded = JSON.load(contents)
-  loaded['files'].each do |file|
-    buffer.ins "#{file['path']}\n" 
+  loaded['files'].reject { |e| e['offenses'].empty? }.each do |file|
+    buffer.ins "F:#{file['path']}\n" 
     file['offenses'].each do |offense|
-      buffer.ins "#{offense['message']}:L:#{offense['location']['line']}:C:#{offense['location']['column']}:#{offense['cop_name']}\n"
+      buffer.ins "\t#{offense['message']}:L:#{offense['location']['line']}:C:#{offense['location']['column']}:#{offense['cop_name']}\n"
     end
   end
 
